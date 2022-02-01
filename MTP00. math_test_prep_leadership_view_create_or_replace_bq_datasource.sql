ce75589614ad88d9790e1f228afcf75e920e454f
David White | msmdesign.nyc <david@msmdesign.nyc>
2022-02-01 13:06:48 -0500

    Add files via upload

---
+++ MTP00. math_test_prep_leadership_view_create_or_replace_bq_datasource.sql
@@ -0,0 +1,37 @@
+--refresh each grade level datasource
+
+SELECT * FROM `harlemlinksy2122.datasources.math_tp_grade3`  ;
+
+SELECT * FROM `harlemlinksy2122.datasources.math_tp_grade4`  ;
+
+SELECT * FROM `harlemlinksy2122.datasources.math_tp_grade5`  ; 
+
+
+--create math test prep leadership view datasource
+
+CREATE OR REPLACE TABLE 
+`harlemlinksy2122.datasources.math_tp_leadership` AS
+
+SELECT *,
+  CASE 
+    WHEN student IS NOT NULL THEN 'SY 2021–22'
+  END AS academic_year
+FROM `harlemlinksy2122.datasources.math_tp_grade3`
+
+
+UNION ALL
+SELECT *,
+ CASE 
+    WHEN student IS NOT NULL THEN 'SY 2021–22'
+  END AS academic_year
+FROM `harlemlinksy2122.datasources.math_tp_grade4`
+
+
+UNION ALL
+SELECT *,
+ CASE 
+    WHEN student IS NOT NULL THEN 'SY 2021–22'
+  END AS academic_year
+FROM `harlemlinksy2122.datasources.math_tp_grade5`  ;
+
+
